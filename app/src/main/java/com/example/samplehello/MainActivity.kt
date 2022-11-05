79c1cac15d54fb327dd12ce4e53a79486d212817
div-antk <51283468+owaii@users.noreply.github.com>
2022-11-05 23:59:00 +0900

    パディングを設定

--- app/src/main/java/com/example/samplehello/MainActivity.kt
+++ app/src/main/java/com/example/samplehello/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.samplehello.ui.theme.SampleHelloTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +36,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Surface(color = Color.Red) {
-        Text(text = "私の名前は $name です")
+        Text(text = "私の名前は $name です", modifier = Modifier.padding(24.dp))
     }
 }
 
